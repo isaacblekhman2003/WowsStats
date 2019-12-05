5c4cc7be9a08854496d6bc02d75d61a6df8db108
Isaacblekhman2003 <isaacblekhman14@gmail.com>
2019-12-05 12:21:00 -0800

    Some intial setup

--- app/src/main/java/com/example/wowsstats/TierXFragment.java
+++ app/src/main/java/com/example/wowsstats/TierXFragment.java
@@ -12,13 +12,35 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TierXFragment extends Fragment {
+
     private ArrayAdapter<Ship> shipArrayAdapter;
+    private Gson gson;
+    private Ship[] ships;
+    private List<Ship> shiplist;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tier_x,container,false);
+        gson = new Gson();
+        InputStream jsonFileInputStream   = getResources().openRawResource(R.raw.shipsX);
+        String json = readTextFile(jsonFileInputStream);
 
+        ships =  gson.fromJson(json, Ship[].class);
+
+        shiplist = Arrays.asList(ships);
+
+        shipArrayAdapter = new HeroAdapter(heroList);
+        wirewidgets();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        heroview.setAdapter(heroesArrayAdapter);
 
 
 
